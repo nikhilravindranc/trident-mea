da6f7d9dcfa36467d8b92a31efaf0e05dc5e8a51
Nikhi <dmops@socialdnalabs.com>
2026-06-23 17:40:09 +0530

    Remove Explore link from Core Capabilities tabs
    The capabilities in the tabbed interface don't have designated pages yet,
    so remove the 'Explore this solution' link from the tab-copy section.
    Bump script version to v5.

--- script-v2.js
+++ script-v2.js
@@ -168,7 +168,6 @@
         '<h3>' + t.h + '</h3>' +
         '<p>' + t.p + '</p>' +
         '<ul class="tab-feats">' + feats + '</ul>' +
-        '<a class="link-arrow" href="#cta">Explore this solution <svg width="16" height="16" viewBox="0 0 24 24" fill="none"><path d="M5 12h14M13 6l6 6-6 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></a>' +
       '</div>' +
       '<div class="tab-visual"><div class="spot">' +
         '<div class="spot-card">' +
